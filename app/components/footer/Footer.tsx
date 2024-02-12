a45fef76759ce1f6786a1a7ffb71a222c2ea3c6a
Hikmet Can Öyke <hikmetcanoyke@outlook.com>
2024-02-12 17:58:10 +0300

    add:project setup

---
+++ app/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="w-full h-24 bg bg-orange-600 text-slate-100 flex items-center justify-center mt-3">
+      hikmetcanoyke@outlook.com
+    </div>
+  );
+};
+
+export default Footer;
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images:{
+        domains: ["cdn.akakce.com"]
+    }
+};
 
 export default nextConfig;
