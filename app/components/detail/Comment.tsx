64db050ecd30fae1ed7fb26c8dd7c6dce709db8e
Hikmet Can Öyke <hikmetcanoyke@outlook.com>
2024-02-13 13:52:17 +0300

    add : detail

---
+++ app/components/detail/Comment.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import { RxAvatar } from "react-icons/rx";
+import Avatar from "../general/Avatar";
+
+const Comment = ({ prd }: { prd: any }) => {
+  return (
+    <div className="border w-full md:w-1/2 p-2 mt-2 rounded-lg">
+      {/* <Avatar image={prd?.user?.image} /> */}
+      <div className="flex items-center gap-1">
+        <RxAvatar size="25" />
+        <div>{prd?.user?.name}</div>
+      </div>
+      <div className="text-slate-500 ">{prd.comment}</div>
+    </div>
+  );
+};
+
+export default Comment;
