af2b35cb20cd6cf4e5e40058e4686ff4090fef9a
Hikmet Can Öyke <hikmetcanoyke@outlook.com>
2024-02-14 15:46:08 +0300

    feat: navbar Search updated.

--- app/components/navbar/Search.tsx
+++ app/components/navbar/Search.tsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const Search = () => {
-  return <div className="flex-1 hidden md:flex">Search</div>;
+  return (
+    <div className="flex-1 hidden md:flex">
+      <input
+        className="py-2 px-3 border-none outline-none flex flex-1 text-black"
+        placeholder="Ne Aramak İstemiştiniz..."
+        type="text"
+      />
+      <button className=" p-2 bg-orange-800 text-sm border border-transparent">
+        Ara
+      </button>
+    </div>
+  );
 };
 
 export default Search;
