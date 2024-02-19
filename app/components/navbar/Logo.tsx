c2de80ec41186f7b35dbc90f9e974f54ea556f60
Hikmet Can Öyke <hikmetcanoyke@outlook.com>
2024-02-19 16:59:20 +0300

    feat: updated logo components

--- app/components/navbar/Logo.tsx
+++ app/components/navbar/Logo.tsx
@@ -1,10 +1,18 @@
-"use client"
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Logo = () => {
+  const router = useRouter();
+
   return (
-    <div className="bg-orange-700 px-2 py-1 rounded-md text-lg md:text-2xl cursor-pointer">Burada<span className="text-sm">.com</span></div>
-  )
+    <div
+      onClick={() => router.push("/")}
+      className="bg-orange-700 px-2 py-1 rounded-md text-lg md:text-2xl cursor-pointer"
+    >
+      Burada<span className="text-sm">.com</span>
+    </div>
+  );
 };
 
 export default Logo;
